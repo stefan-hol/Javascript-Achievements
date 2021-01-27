7678b66a5561777a0728578bf63540aa114c726c
stefan <stefan.hol@hotmail.com>
2021-01-27 14:22:14 +0100

    nog een keer want ging fout

---
+++ les5/quize/quiz1.js
@@ -0,0 +1,77 @@
+const quiz1 =
+{
+    "quizID": "Museum 1",
+    "quizMetaData":
+    {
+        "title": "Rekenquiz!",
+        "imageURI": "https://static.wikia.nocookie.net/disney/images/9/97/Perry_The_Platypus_2.png/revision/latest/scale-to-width-down/340?cb=20110527111214"
+
+    },
+    "quizContent":		
+		[
+			{
+				"question": "Wat is 2+2?",
+				"answers": [{
+						"answer": "4",
+						"feedback": true
+					},
+					{
+						"answer": "5",
+						"feedback": false
+					},
+					{
+						"answer": "6",
+						"feedback": false
+					},
+					{
+						"answer": "7",
+						"feedback": false
+					},
+					{
+						"answer": "8",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "Wat is 2 X 2?",
+				"answers": [{
+						"answer": "6",
+						"feedback": false
+					},
+					{
+						"answer": "4",
+						"feedback": true
+					},
+					{
+						"answer": "9",
+						"feedback": false
+					},
+					{
+						"answer": "8",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "Wat is 12 : 3?",
+				"answers": [{
+						"answer": "6",
+						"feedback": false
+					},
+					{
+						"answer": "8",
+						"feedback": false
+					},
+					{
+						"answer": "3",
+						"feedback": false
+					},
+					{
+						"answer": "4",
+						"feedback": true
+					}
+				]
+			}
+		]
+	}
---
+++ les5/quize/quiz2.js
@@ -0,0 +1,65 @@
+const quiz2 = 
+{
+    "quizID": "Math 1",
+    "quizMetaData":
+    {
+        "title": "Math Trivia  1",
+        "imageURI": "https://www.animaatjes.nl/plaatjes/p/perry-het-vogelbekdier/animaatjes-perry-het-vogelbekdier-15243.png"
+
+    },
+    "quizContent":		
+		[
+			{
+				"question": "3x3",
+				"answers": [{
+						"answer": "9",
+						"feedback": true
+					},
+					{
+						"answer": "10",
+						"feedback": false
+					},
+					{
+						"answer": "11",
+						"feedback": false
+					},
+					{
+						"answer": "13",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "6 - 4 =  ?",
+				"answers": [{
+						"answer": "1",
+						"feedback": false
+					},
+					{
+						"answer": "2",
+						"feedback": true
+					},
+					{
+						"answer": "3",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "3 + 8 = 8",
+				"answers": [{
+						"answer": "no",
+						"feedback": true
+					},
+					{
+						"answer": "sometimes",
+						"feedback": false
+					},
+					{
+						"answer": "yes",
+						"feedback": false
+					}
+				]
+			}
+		]
+	}
